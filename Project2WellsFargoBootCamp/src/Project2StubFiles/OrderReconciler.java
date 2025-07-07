7edc7ea8f378e80e026bd9a1b983d1ab4dc9508c
cherianabe <150985577+cherianabe@users.noreply.github.com>
2025-07-07 19:58:42 -0400

    Update OrderReconciler.java
    Finished main method to compare the client order to the child order. Needs further implementation.

--- Project2WellsFargoBootCamp/src/Project2StubFiles/OrderReconciler.java
+++ Project2WellsFargoBootCamp/src/Project2StubFiles/OrderReconciler.java
@@ -1,13 +1,25 @@
 package Project2StubFiles;
 
-public class OrderReconciler {
-	
-	//method to reconcile order. 
-	//method will be added later
-	
-	public void reconcileOrder()
-	{
-		//changes here
-	}
+import java.util.List;
 
+public class OrderReconciler {
+
+    public void reconcile(List<ClientOrder> clientOrders, List<ChildOrder> childOrders) {
+        for (ClientOrder client : clientOrders) {
+            int totalFilled = 0;
+
+            for (ChildOrder child : childOrders) {
+                if (child.getParentId().equals(client.getClientOrderID())) {
+                    totalFilled += child.getFilledQuantity();
+                }
+            }
+
+            client.increaseFilledQuantity(totalFilled);
+
+            if (client.getFilledQuantity() > client.getQuantity()) {
+                System.out.println("Overfill detected for Client Order " + client.getClientOrderID()
+                        + " | Filled: " + client.getFilledQuantity() + " / " + client.getQuantity());
+            }
+        }
+    }
 }
