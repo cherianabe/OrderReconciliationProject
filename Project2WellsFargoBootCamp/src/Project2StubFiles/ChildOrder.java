82cdda7cd20fad7ee354ff563644efa5118cf99c
MuhammadWaqas9195 <drwaqasfeb19@gmail.com>
2025-07-11 08:06:23 -0400

    Update ChildOrder.java

--- Project2WellsFargoBootCamp/src/Project2StubFiles/ChildOrder.java
+++ Project2WellsFargoBootCamp/src/Project2StubFiles/ChildOrder.java
@@ -1,58 +1,54 @@
+ChildOrder.java
 package Project2StubFiles;
 
 public class ChildOrder {
-	public String id, parentID, symbol, side;
-	public int quantity, filledQuantity;
-	
-	//constructor to set all fields!
-	//may need change to access parentID string... don't know right now. further implementation
-	
-	public ChildOrder(String id, String parentID, String symbol, String side, int quantity, int filledQuantity)
-	{
-		this.id = id;
-		this.parentID = parentID;
-		this.symbol = symbol;
-		this.side = side;
-		this.quantity = quantity;
-		this.filledQuantity = filledQuantity;
-	}
-	
-	//getters for all fields!
-	public String getID()
-	{
-		return id;
-		return parentID;
-	}
-	
-	public String getParentID()
-	{
-		return parentID;
-	}
-	
-	public String getSymbol()
-	{
-		return symbol;
-	}
-	
-	public String getSide()
-	{
-		return side;
-	}
-	
-	public int getQuantity()
-	{
-		return quantity;
-	}
-	
-	public int getFilledQuantity()
-	{
-		return filledQuantity;
-	}
-	
-	//toString to print the fields
-	public String toString()
-	{
-		return "ID: " +id+ " ParentID: "+parentID+" Symbol: "+symbol+" Side: " +side+ " Quantity: "+quantity+" Filled Quantity: "+filledQuantity;
-	}
+    public String id, parentID, symbol, side;
+    public int quantity, filledQuantity;
 
+    // Constructor initializes all fields when an object is created
+    public ChildOrder(String id, String parentID, String symbol, String side, int quantity, int filledQuantity) {
+        this.id = id;
+        this.parentID = parentID;
+        this.symbol = symbol;
+        this.side = side;
+        this.quantity = quantity;
+        this.filledQuantity = filledQuantity;
+    }
+
+    // Getter for child order ID
+    public String getID() {
+        return id;
+    }
+
+    // Getter for parent (client) order ID
+    public String getParentID() {
+        return parentID;
+    }
+
+    // Getter for stock symbol
+    public String getSymbol() {
+        return symbol;
+    }
+
+    // Getter for side (BUY or SELL)
+    public String getSide() {
+        return side;
+    }
+
+    // Getter for order quantity
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Getter for how much quantity has been filled
+    public int getFilledQuantity() {
+        return filledQuantity;
+    }
+
+    // Converts object to readable string
+    public String toString() {
+        return "ID: " + id + " ParentID: " + parentID + " Symbol: " + symbol + 
+               " Side: " + side + " Quantity: " + quantity + " Filled Quantity: " + filledQuantity;
+    }
 }
+
