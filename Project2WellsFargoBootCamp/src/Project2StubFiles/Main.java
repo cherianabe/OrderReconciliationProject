754930ae2c3289ce74f5d5efbdf17d0312561086
cherianabe <150985577+cherianabe@users.noreply.github.com>
2025-07-11 09:53:08 -0400

    Update Main.java

--- Project2WellsFargoBootCamp/src/Project2StubFiles/Main.java
+++ Project2WellsFargoBootCamp/src/Project2StubFiles/Main.java
@@ -1,10 +1,23 @@
-package Project2StubFiles;
+package July11FinishedBuild;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	public static void main(String[] args)
-	{
-		//main method only
-	}
+    public static void main(String[] args) {
+        try {
+            String clientFile = "client_order.csv";
+            String childFile = "child_order.csv";
 
+            List<ClientOrder> clientOrders = OrderReader.readClientOrders("client_order.csv");
+            List<ChildOrder> childOrders = OrderReader.readChildOrders("child_order.csv");
+
+            OrderReconciler.reconcile(clientOrders, childOrders);
+
+            ReportGenerator.generateReport(clientOrders);
+
+        } catch (IOException e) {
+            System.out.println("Error reading files: " + e.getMessage());
+        }
+    }
 }
